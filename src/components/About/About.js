eb9793fdf51adcfe04207eec2a94afe83f6b22ba
Pratyush Prasoon <154238801+pratyush3300@users.noreply.github.com>
2025-09-22 19:11:24 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -29,7 +29,7 @@ function About() {
             <br /><br />
             
             <ScrollAnimation animateIn="fadeInLeft">
-            An articulate and relentless individual with strong problem-solving and interpersonal skills. Quick learner with a drive to explore new technologies. Passionate about learning different coding languages. Collaboration is key of my approach, and I enjoy working with cross-functional teams to create solutions that not only meet technical requirements but also enhance user experience and deliver real value.
+            I identify myself as an articulate and relentless individual with strong problem-solving and interpersonal skills. Quick learner with a drive to explore new technologies. Passionate about learning different coding languages. Collaboration is key of my approach, and I enjoy working with cross-functional teams to create solutions that not only meet technical requirements but also enhance user experience and deliver real value.
             My hobbies are playing sports and travelling to different places. I like to treat people with respect and enjoy helping them.
             </ScrollAnimation>
 
